f320538424c915c50a5f081ffbd840d8f5ec9818
Ranbut <Ranbut_Beater@protonmail.com>
2022-10-28 11:42:13 -0300

    Adicionado script e axios

---
+++ script.js
@@ -0,0 +1,10 @@
+let mensagens = [];
+let participantes = [];
+
+function mostrarMensagens(){
+    //Exibir messagens no HTML
+}
+
+function mostrarParticipantes(){
+    //Exibir participantes no HTML
+}
