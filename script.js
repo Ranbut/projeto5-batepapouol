21fecf346ac6b0973090b5ecf1b20fc7ce61049d
Ranbut <Ranbut_Beater@protonmail.com>
2022-10-28 11:48:48 -0300

    Adicionado prompt de nome e envio de dados

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 let mensagens = [];
 let participantes = [];
 
+perguntarNome();
+
+function perguntarNome() {
+  nome = prompt("Digite seu nome para começarmos:");
+  registrarParticipante(nome);
+}
+
+function registrarParticipante(nome){
+    const dados = { name: nome };
+    const requisicao = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", dados);
+    requisicao.then(entrarNoChat);
+    requisicao.catch(perguntarNome);
+    //Colocar no servidor dados do participante atual   
+}
+
+function entrarNoChat(){
+    
+}
+
 function mostrarMensagens(){
     //Exibir messagens no HTML
 }
 
-function mostrarParticipantes(){
+function mostrarParticipantes(){        
     //Exibir participantes no HTML
 }
